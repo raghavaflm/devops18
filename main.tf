b3cf5eb4ed0bc3440c7ce1d6dc1483a15b8fe0df
raghavaflm <raghuat1991@gmail.com>
2024-10-17 21:33:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,4 @@
-resource "aws_launch_template" "web_server_as" {
-    name = "myproject"
-    image_id           = "ami-00f251754ac5da7f0"
-    instance_type = "t2.micro"
-    key_name = "mustafakey-pair"
-    
-}
-   
-
-
-  resource "aws_elb" "web_server_lb"{
-     name = "web-server-lb"
-     security_groups = [aws_security_group.web_server.id]
-     subnets = ["subnet-0883b34c988bcbf20", "subnet-0f4f4322d734fcc38"]
+subnets = ["subnet-0883b34c988bcbf20", "subnet-0f4f4322d734fcc38"]
      listener {
       instance_port     = 8000
       instance_protocol = "http"
